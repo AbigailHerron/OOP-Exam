327fc71616492fd6a4d3943850fd473ca57f35fc
AbigailHerron <abigailherron@hotmail.com>
2021-01-11 10:51:00 +0000

    Update MainWindow.xaml.cs

--- OOP Exam S00200536/OOP Exam S00200536/MainWindow.xaml.cs
+++ OOP Exam S00200536/OOP Exam S00200536/MainWindow.xaml.cs
@@ -11,6 +11,7 @@
  ##########################################################################################################################*/
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,9 +32,18 @@ namespace OOP_Exam_S00200536
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*VARIABLES -------------------------------------------------------------------------------------------------------*/
+        ObservableCollection<Account> accounts = new ObservableCollection<Account>();
+        ObservableCollection<Account> filteredAccounts = new ObservableCollection<Account>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lbxAccounts.ItemsSource = accounts;
         }
     }
 }
